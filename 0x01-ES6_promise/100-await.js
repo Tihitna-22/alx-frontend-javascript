761b4259598045cec6dc24eac31589bcdd0548e0
tihitna sisay <tihitna222sisay@gmail.com>
2022-06-26 23:28:23 +0300

    modify for style

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,12 +3,15 @@ import { uploadPhoto, createUser } from './utils';
 const arg1 = uploadPhoto;
 const arg2 = createUser;
 export default async function asyncUploadUser() {
-  return Promise.all([arg1(), arg2()]).then((value) => ({
-    photo: value[0],
-    user: value[1],
-  }))
-    .catch(() => ({
+  try {
+    return Promise.all([arg1(), arg2()]).then((value) => ({
+      photo: value[0],
+      user: value[1],
+    }));
+  } catch (error) {
+    return {
       photo: null,
       user: null,
-    }));
+    };
+  }
 }
